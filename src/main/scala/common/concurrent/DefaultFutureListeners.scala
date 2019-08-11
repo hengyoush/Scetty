c05025c3702271280e4b09dd70aa6ccdf5489b8d
hengyoush <hengyoush1@163.com>
2019-08-11 12:54:18 +0800

    增加DefaultPromise支持作为Promise的实现

---
+++ src/main/scala/common/concurrent/DefaultFutureListeners.scala
@@ -0,0 +1,44 @@
+package common.concurrent
+
+/**
+  * thread not safe
+  */
+final class DefaultFutureListeners {
+  private var _listeners: Array[GenericFutureListener[_ <: Future[_]]] = _
+  private var size: Int = _listeners.length
+
+  def this(first: GenericFutureListener[_ <: Future[_]], second: GenericFutureListener[_ <: Future[_]]) = {
+    this()
+    _listeners = Array(first, second)
+    size = _listeners.length
+  }
+
+  def add(l: GenericFutureListener[_ <: Future[_]]): Unit = {
+    if (size == _listeners.length) {
+      this._listeners = Array.copyOf(this._listeners, size << 1)
+    }
+    this._listeners(size) = l
+    this.size += 1
+  }
+
+  def remove(toRemove: GenericFutureListener[_ <: Future[_]]): Unit = {
+    _listeners indexOf toRemove match {
+      case -1 =>
+      case i =>
+        val listenersToMove = size - i - 1
+        // listenersToMove可能为0，因为移除的listener在数组的最后一个
+        if (listenersToMove > 0) {
+          Array.copy(_listeners, 1 + i, _listeners, i, listenersToMove)
+        }
+        size -= 1
+        _listeners(size) = null
+    }
+  }
+
+  def listeners: Array[GenericFutureListener[_ <: Future[_]]] = _listeners
+}
+
+object DefaultFutureListeners {
+  def apply(first: GenericFutureListener[_ <: Future[_]], second: GenericFutureListener[_ <: Future[_]]): DefaultFutureListeners
+  = new DefaultFutureListeners(first, second)
+}
--- src/main/scala/common/concurrent/DefaultPromise.scala
+++ src/main/scala/common/concurrent/DefaultPromise.scala
@@ -1,18 +1,20 @@
 package common.concurrent
 
+import java.util.concurrent.TimeUnit.MILLISECONDS
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater
 
 import common.SystemPropertyUtil
+import common.concurrent.DefaultPromise._
 import common.logger.{InternalLogger, InternalLoggerFactory}
 import eventloop.EventExecutor
-import DefaultPromise._
 
-import scala.runtime.VolatileObjectRef
+import scala.concurrent.CancellationException
+import scala.concurrent.duration.TimeUnit
 
 class DefaultPromise[T](eventExecutor: EventExecutor) extends AbstractFuture[T] with Promise[T] {
   require(eventExecutor != null, "eventExecutor不能为空")
 
-  private var result: VolatileObjectRef[AnyRef] = _
+  @volatile private var result: Any = _
 
   private var listeners: AnyRef = _
   private var waiters: Short = _
@@ -20,15 +22,147 @@ class DefaultPromise[T](eventExecutor: EventExecutor) extends AbstractFuture[T] 
 
   protected def this() = this(null)
 
-  override def setSuccess(result: T): Promise[T] = {
+  override def setSuccess(result: T): Promise[T] =
+    if (setSuccess0(result)) this else throw new IllegalStateException("operation already success")
 
+  override def trySuccess(result: T): Boolean = setSuccess0(result)
+
+  override def setFailure(cause: Throwable): Promise[T] =
+    if (setFailure0(cause)) this else throw new IllegalStateException("operation already success")
+
+  override def tryFailure(cause: Throwable): Boolean = setFailure0(cause)
+
+  override def setUncancellable: Boolean = {
+    if (RESULT_UPDATER.compareAndSet(this, null, UNCANCELLABLE))
+      true
+    else {
+      val result = this.result
+      !isDone0(result) || !isCancelled0(result)
+    }
   }
 
-  private def setSuccess0(result: T): Boolean = {
+  override def isDone: Boolean = isDone0(result)
 
+  override def isSuccess: Boolean = {
+    val result = this.result
+    result != null && result != UNCANCELLABLE && !result.isInstanceOf[CauseHolder]
   }
 
-  private def setValue0(result: AnyRef): Boolean = {
+  override def isCancelled: Boolean = isCancelled0(this.result)
+
+  override def isCancellable: Boolean = result == null
+
+  override def cause: Throwable = {
+    val result = this.result
+    result match {
+      case r: CauseHolder => r.cause
+      case _ => null
+    }
+  }
+
+  override def addListener(listener: GenericFutureListener[_ <: Future[_ >: T]]): Promise[T] = {
+    synchronized(this)(addListener0(listener))
+    if (isDone) notifyListeners()
+    this
+  }
+
+  override def addListeners(listeners: GenericFutureListener[_ <: Future[_ >: T]]*): Promise[T] = {
+    synchronized(this)(listeners.filter(_ != null).foreach(addListener0))
+    if (isDone) notifyListeners()
+    this
+  }
+
+  override def removeListener(listener: GenericFutureListener[_ <: Future[_ >: T]]): Promise[T] = {
+    synchronized(this)(removeListener0(listener))
+    this
+  }
+
+  override def removeListeners(listeners: GenericFutureListener[_ <: Future[_ >: T]]*): Promise[T] = {
+    synchronized(this)(listeners.filterNot(_ == null).foreach(removeListener0))
+    this
+  }
+
+  override def await: Promise[T] = {
+    if (isDone) return this
+    if (Thread.interrupted()) throw new InterruptedException
+
+    checkDeadLock()
+
+    synchronized(this) {
+      while (!isDone) {
+        incWaiters()
+        try wait()
+        finally decWaiters()
+      }
+    }
+    this
+  }
+
+  override def awaitUninterruptibly: Promise[T] = {
+    if (isDone) return this
+
+    checkDeadLock()
+
+    var interrupted = false
+    synchronized(this) {
+      while (!isDone) {
+        incWaiters()
+        try wait()
+        catch {
+          case InterruptedException => interrupted = true
+        }
+        finally decWaiters()
+      }
+    }
+
+    if (interrupted) Thread.currentThread().interrupt()
+    this
+  }
+
+  override def await(timeout: Long, unit: TimeUnit): Boolean = await0(unit.toNanos(timeout), interruptable = true)
+
+  override def await(timeoutMills: Long): Boolean = await0(MILLISECONDS.toNanos(timeoutMills), interruptable = true)
+
+  override def awaitUninterruptibly(timeoutMills: Long, unit: TimeUnit): Boolean =
+    await0(unit.toNanos(timeoutMills), interruptable = false)
+
+  override def awaitUninterruptibly(timeoutMills: Long): Boolean =
+    await0(MILLISECONDS.toNanos(timeoutMills), interruptable = false)
+
+  override def getNow: T = {
+    this.result match {
+      case CauseHolder => _
+      case SUCCESS => _
+      case UNCANCELLABLE => _
+      case _ => result.asInstanceOf[T]
+    }
+  }
+
+  override def cancel(mayInterruptIfRunning: Boolean): Boolean = {
+    if (RESULT_UPDATER.get(this) == null &&
+      RESULT_UPDATER.compareAndSet(this, null, CauseHolder(new CancellationException))) {
+      if (checkNotifyWaiters) notifyListeners()
+      return true
+    }
+
+    false
+  }
+
+  override def sync: Promise[T] = {
+    await
+    if (cause == null) this else throw cause
+  }
+
+  override def syncUninterruptibly: Promise[T] = {
+    awaitUninterruptibly
+    if (cause == null) this else throw cause
+  }
+
+  private def setSuccess0(result: T): Boolean = setValue0(if (result == null) SUCCESS else result)
+
+  private def setFailure0(cause: Throwable): Boolean = setValue0(CauseHolder(cause))
+
+  private def setValue0(result: Any): Boolean = {
     if (RESULT_UPDATER.compareAndSet(this, null, result)
       || RESULT_UPDATER.compareAndSet(this, UNCANCELLABLE, result)) {
       if (checkNotifyWaiters) {
@@ -41,11 +175,9 @@ class DefaultPromise[T](eventExecutor: EventExecutor) extends AbstractFuture[T] 
     false
   }
 
-  private def checkNotifyWaiters: Boolean = {
-    synchronized(this) {
-      if (waiters > 0) notifyAll()
-      listeners != null
-    }
+  private def checkNotifyWaiters: Boolean = synchronized(this) {
+    if (waiters > 0) notifyAll()
+    listeners != null
   }
 
   private def notifyListeners(): Unit = {
@@ -60,6 +192,88 @@ class DefaultPromise[T](eventExecutor: EventExecutor) extends AbstractFuture[T] 
   }
 
   private def notifyListenersNow(): Unit = {
+    var listeners: AnyRef = null
+    synchronized(this) {
+      if (notifyingListeners || this.listeners == null)
+        return
+      notifyingListeners = true
+      listeners = this.listeners
+      this.listeners = null
+    }
+
+    while (true) {
+      listeners match {
+        case _: DefaultFutureListeners => notifyListeners0 _
+        case _: GenericFutureListener[_ <: Future[_ >: T]] => notifyListener0(this, _)
+        case _ => throw new IllegalStateException("cant reach here")
+      }
+
+      synchronized(this) {
+        listeners = this.listeners
+        this.listeners = null
+
+        if (listeners == null) {
+          notifyingListeners = false
+          return
+        }
+      }
+    }
+  }
+
+  private def notifyListeners0(l: DefaultFutureListeners): Unit = l.listeners.foreach(notifyListener0(this, _))
+
+  private def addListener0(l: GenericFutureListener[_]): Unit = {
+    listeners match {
+      case null => this.listeners = l
+      case listeners: DefaultFutureListeners => listeners add l
+      case listener: GenericFutureListener[_] => this.listeners = DefaultFutureListeners(listener, l)
+      case _ =>
+    }
+  }
+
+  private def removeListener0(l: GenericFutureListener[_]): Unit = {
+    listeners match {
+      case listeners: DefaultFutureListeners => listeners.remove(l)
+      case `l` => this.listeners = null
+      case _ =>
+    }
+  }
+
+  private def checkDeadLock(): Unit = {
+    if (eventExecutor != null && eventExecutor.inEventLoop) throw new IllegalStateException("dead lock")
+  }
+
+  private def incWaiters(): Unit =
+    if (waiters == Short.MaxValue) throw new IllegalStateException("too many waiters") else waiters += 1
+
+  private def decWaiters(): Unit = waiters -= 1
+
+  private def await0(timeoutNano: Long, interruptable: Boolean): Boolean = {
+    if (isDone) return true
+    if (timeoutNano <= 0) return isDone
+    if (interruptable && Thread.interrupted()) throw new InterruptedException
+
+    checkDeadLock()
+
+    var interrupted, break = false
+    var waitTime = timeoutNano
+    val startTime = System.nanoTime()
+    try {
+      while (!break) {
+        synchronized(this) {
+          incWaiters()
+          try wait(waitTime / 1000000, Int(waitTime % 1000000))
+          catch {
+            case InterruptedException => if (interruptable) throw _ else interrupted = true
+          }
+          finally decWaiters()
+        }
+        if (isDone) break = true
+        waitTime = timeoutNano - (System.nanoTime() - startTime)
+        if (waitTime < 0) break = true
+      }
+      isDone
+    } finally if (interrupted) Thread.currentThread().interrupt()
 
   }
 }
@@ -71,9 +285,27 @@ object DefaultPromise {
     SystemPropertyUtil.getInt("io.netty.defaultPromise.maxListenerStackDepth", 8))
   private val currStackDepth = ThreadLocal.withInitial[Int](() => 0)
 
-  private val RESULT_UPDATER: AtomicReferenceFieldUpdater[DefaultPromise[_], AnyRef] =
+  private val RESULT_UPDATER: AtomicReferenceFieldUpdater[DefaultPromise[_], Any] =
     AtomicReferenceFieldUpdater.newUpdater(DefaultPromise.getClass, AnyRef.getClass, "result")
   private val SUCCESS = new AnyRef
   private val UNCANCELLABLE = new AnyRef
+
+  private def notifyListener0[V](f: Future[_ >: V], l: GenericFutureListener[_ <: Future[_ >: V]]): Unit = l operationComplete f
+
+  private def isDone0(result: Any): Boolean = result != null && result != UNCANCELLABLE
+
+  private def isCancelled0(result: Any): Boolean =
+    result match {
+      case r: CauseHolder => r.cause.isInstanceOf[CancellationException]
+      case _ => false
+    }
+
+  private class CauseHolder(_cause: Throwable) {
+    def cause: Throwable = _cause
+  }
+
+  object CauseHolder {
+    def apply(cause: Throwable): CauseHolder = new CauseHolder(cause)
+  }
 
 }
