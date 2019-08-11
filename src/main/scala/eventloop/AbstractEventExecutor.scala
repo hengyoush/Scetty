9a3f339fce62dafc2f0ae89a82e67a538e8a41a1
hengyoush <hengyoush1@163.com>
2019-08-11 18:20:45 +0800

    完善AbstractEventExecutor

--- src/main/scala/eventloop/AbstractEventExecutor.scala
+++ src/main/scala/eventloop/AbstractEventExecutor.scala
@@ -1,11 +1,11 @@
 package eventloop
 
 import java.util.concurrent.TimeUnit.SECONDS
-import java.util.concurrent.{AbstractExecutorService, TimeUnit}
+import java.util.concurrent.{AbstractExecutorService, Callable, RunnableFuture, TimeUnit}
 
-import common.concurrent.Future
+import common.concurrent.{DefaultPromise, Future, Promise, PromiseTask}
 import common.logger.InternalLoggerFactory
-import AbstractEventExecutor._
+import eventloop.AbstractEventExecutor._
 
 abstract class AbstractEventExecutor(_parent: EventExecutorGroup)
   extends AbstractExecutorService with EventExecutor {
@@ -16,12 +16,36 @@ abstract class AbstractEventExecutor(_parent: EventExecutorGroup)
 
   override def parent: EventExecutorGroup = _parent
   override def next: EventExecutor = this
+  override def iterator: Iterator[EventExecutor] = selfIte
 
   override def shutdownGracefully: Future[_] = shutdownGracefully(defaultTimeout, defaultQuietPeriod, SECONDS)
+
+  override def newPromise[V]: Promise[V] = DefaultPromise(eventExecutor = this)
+
+  override def submit[T](task: Callable[T]): Future[T] = super.submit(task).asInstanceOf
+  override def submit(task: Runnable): Future[_] = super.submit(task).asInstanceOf
+  override def submit[T](task: Runnable, result: T): Future[T] = super.submit(task, result).asInstanceOf
+
+  override def newTaskFor[T](callable: Callable[T]): RunnableFuture[T] = PromiseTask(this, callable)
+  override def newTaskFor[T](runnable: Runnable, value: T): RunnableFuture[T] = PromiseTask(this, runnable, value)
+
+  override def schedule(command: Runnable, delay: Long, unit: TimeUnit) =
+    throw new UnsupportedOperationException
+
+  override def schedule[V](callable: Callable[V], delay: Long, unit: TimeUnit) =
+    throw new UnsupportedOperationException
+
+  override def scheduleAtFixedRate(command: Runnable, initialDelay: Long, period: Long, unit: TimeUnit) =
+    throw new UnsupportedOperationException
+
+  override def scheduleWithFixedDelay(command: Runnable, initialDelay: Long, delay: Long, unit: TimeUnit) =
+    throw new UnsupportedOperationException
 }
 
 object AbstractEventExecutor {
   private val logger = InternalLoggerFactory.getInstance(AbstractEventExecutor.getClass)
   private val defaultQuietPeriod: Long = 2
   private val defaultTimeout: Long = 15
+
+  protected def safeExecute(task: Runnable): Unit = task.run()
 }
--- src/main/scala/eventloop/EventExecutor.scala
+++ src/main/scala/eventloop/EventExecutor.scala
@@ -1,9 +1,15 @@
 package eventloop
 
+import common.concurrent.{Future, Promise}
+
 trait EventExecutor extends EventExecutorGroup {
   def next: EventExecutor
   def parent: EventExecutorGroup
 
   def inEventLoop: Boolean = inEventLoop(Thread.currentThread)
   def inEventLoop(thread: Thread): Boolean
+
+  def newPromise[V]: Promise[V]
+  def newSucceededFuture[V](result: V): Future[V]
+  def newFailedFuture[V](cause: Throwable): Future[V]
 }
